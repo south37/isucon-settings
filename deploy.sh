8d3bad2be84b52cc62e7f58fb3541cac0d8eec94
Nao Minami <south37777@gmail.com>
2017-10-15 09:51:59 +0900

    Remove unecessary echo

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo 'Update config file...'
-  echo "isucon" | sudo cp "${HOME}/nginx.conf" /etc/nginx/nginx.conf
-  echo "isucon" | sudo cp "${HOME}/my.conf" /etc/mysql/my.cnf
+  sudo cp "${HOME}/nginx.conf" /etc/nginx/nginx.conf
+  sudo cp "${HOME}/my.conf" /etc/mysql/my.cnf
 echo 'Updateed config file!'
 
 echo 'restart services...'
-  echo "isucon" | sudo systemctl restart nginx
+  sudo systemctl restart nginx
   # Save cache
-  # echo "isucon" | sudo systemctl restart mysql
+  # sudo systemctl restart mysql
 
   # Stop
-  echo "isucon" | sudo systemctl stop isu.service
+  sudo systemctl stop isu.service
 
   # Update access log
   ./rotate.sh
 
   # Update
-  echo "isucon" | sudo systemctl start isu.service
+  sudo systemctl start isu.service
 echo 'restarted!'
