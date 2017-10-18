2199ebc66d6de96d8349402ac33f850720a61448
Nao Minami <south37777@gmail.com>
2017-10-18 14:47:53 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ./.emacs.el "$HOME/.emacs.el"
+cp ./.gitconfig "$HOME/.gitconfig"
+cp ./authorized_keys "$HOME/authorized_keys"
+chmod 600 "$HOME/authorized_keys"
+
+# cp ./deploy.sh "$HOME/deploy.sh"
+cp ./rotate.sh "$HOME/rotate.sh"
+cp ./log.sh "$HOME/log"
