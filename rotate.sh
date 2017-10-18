dba2c2e09ad2a58cf4b364c4d4cb9d83849dda54
Nao Minami <south37777@gmail.com>
2017-10-18 14:43:44 +0900

    Update scripts

--- rotate.sh
+++ rotate.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-mv "$HOME/webapp/var/log/nginx/access_kataribe.log" "$HOME/webapp/var/log/nginx/`date +"%Y%m%d%H%M%S"`_access_kataribe.log"
-touch "$HOME/webapp/var/log/nginx/access_kataribe.log"
+mv "/var/log/nginx/access.log" "/var/log/nginx/`date +"%Y%m%d%H%M%S"`_access.log"
