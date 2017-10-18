18e1df09dc59d58a025523677b0997a74ab4281e
Nao Minami <south37777@gmail.com>
2017-10-18 14:50:02 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ cp ./.gitconfig "$HOME/.gitconfig"
 cp ./authorized_keys "$HOME/authorized_keys"
 chmod 600 "$HOME/authorized_keys"
 
-# cp ./deploy.sh "$HOME/deploy.sh"
+cp ./deploy.sh "$HOME/deploy.sh"
 cp ./rotate.sh "$HOME/rotate.sh"
-cp ./log.sh "$HOME/log"
+cp ./log.sh "$HOME/log.sh"
