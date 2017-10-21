a94ca803542f150d51f69d29897440f4d83cb439
Nao Minami <south37777@gmail.com>
2017-10-21 11:18:45 +0900

    Add comment

--- dump.sh
+++ dump.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# dir in redis.conf. `sudo systemctl restart redis` may be necessary
 # -rw-r--r--  1 redis redis   26 Oct 21 11:09 dump.rdb
 sudo cp /var/lib/redis/dump.rdb "$HOME/data/`date +"%Y%m%d%H%M%S"`_redis_dump.rdb"
