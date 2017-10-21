51a2e405f2026e12e48347f5f67654947cf53e69
Nao Minami <south37777@gmail.com>
2017-10-21 12:06:44 +0900

    Add redis

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,13 @@ echo 'Rotated log file!'
 
 echo 'Update config file...'
   sudo cp "${HOME}/nginx.conf" /etc/nginx/nginx.conf
-  sudo cp "${HOME}/redis.conf" /etc/redis/redis.conf
+  # sudo cp "${HOME}/redis.conf" /etc/redis/redis.conf
   # sudo cp "${HOME}/my.conf" /etc/mysql/my.cnf
 echo 'Updateed config file!'
 
 echo 'restart services...'
   sudo systemctl restart nginx.service
+  # sudo systemctl restart redis.service
   # Save cache
   # sudo systemctl restart mysql.service
   sudo systemctl restart isu.ruby.service
