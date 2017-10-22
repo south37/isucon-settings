4cdfb0789c687fc7b6434da5cfd047cbd68118eb
Nao Minami <south37777@gmail.com>
2017-10-22 12:38:48 +0900

    Add --bundle option

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,18 @@ echo 'Rotate log file...'
 echo 'Rotated log file!'
 
 echo 'Update config file...'
-  sudo cp "${HOME}/nginx.conf" /etc/nginx/nginx.conf
-  # sudo cp "${HOME}/redis.conf" /etc/redis/redis.conf
-  # sudo cp "${HOME}/my.conf" /etc/mysql/my.cnf
+  sudo cp "$HOME/nginx.conf" /etc/nginx/nginx.conf
+  # sudo cp "$HOME/redis.conf" /etc/redis/redis.conf
+  # sudo cp "$HOME/my.conf" /etc/mysql/my.cnf
 echo 'Updateed config file!'
+
+if [ $1 = "--bundle" ]; then
+  echo 'Start bundle install...'
+  cd "$HOME/webapp/ruby"
+  bundle install
+  cd "$HOME"
+  echo 'bundle install finished!'
+fi
 
 echo 'Restart services...'
   sudo systemctl restart nginx.service
