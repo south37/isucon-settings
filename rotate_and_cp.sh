411da3270cc2c74f259654f1f9dfae0037a080b4
Nao Minami <south37777@gmail.com>
2017-10-22 10:56:13 +0900

    Fix typo

--- rotate_and_cp.sh
+++ rotate_and_cp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-$logfile="`date +"%Y%m%d%H%M%S"`_access.log"
-sudo mv "/var/log/nginx/access.log" "/var/log/nginx/$logfile"
-sudo cp "/var/log/nginx/$logfile" "log/$logfile"
+LOGFILE="`date +"%Y%m%d%H%M%S"`_access.log"
+sudo mv "/var/log/nginx/access.log" "/var/log/nginx/$LOGFILE"
+sudo cp "/var/log/nginx/$LOGFILE" "log/$LOGFILE"
+sudo chown isucon:isucon "log/$LOGFILE"
 sudo systemctl restart nginx
