e18f4bf5f94b9604f3a973f01184adca43fddbd6
Nao Minami <south37777@gmail.com>
2017-11-24 23:19:42 +0900

    Add nginx

--- all_scripts/log.sh
+++ all_scripts/log.sh
@@ -3,6 +3,7 @@
 . "$(pwd)/all_scripts/hosts.txt"
 
 TAIL_LENGTH=10
+TARGETS="nginx app"
 
 while [ "$1" != "" ]; do
   case "$1" in
@@ -10,16 +11,32 @@ while [ "$1" != "" ]; do
         TAIL_LENGTH=$2
         shift 2
         ;;
+    '-t' )
+        TARGETS=$2
+        shift 2
+        ;;
     * )
-        shift 1
+        shift
         ;;
   esac
 done
 
-COMMAND="hostname && ( /home/isucon/scripts/log_app.sh | tail -n ${TAIL_LENGTH} )"
-for i in ${HOSTS[@]}; do
-  echo ""
-  echo $i
-  ssh "isucon@${i}" "${COMMAND}"
-done
-echo ""
+NGINX_COMMAND="hostname && echo '' && ( /home/isucon/scripts/log_nginx.sh | tail -n ${TAIL_LENGTH} )"
+# nginx
+if [ "${TARGETS}" == *"nginx"* ]; then
+  for i in ${NGINX_HOSTS[@]}; do
+    echo ""
+    echo $i
+    ssh "isucon@${i}" "${NGINX_COMMAND}"
+  done
+fi
+
+APP_COMMAND="hostname && echo '' && ( /home/isucon/scripts/log_app.sh | tail -n ${TAIL_LENGTH} )"
+# app
+if [ "${TARGETS}" == *"app"* ]; then
+  for i in ${WEB_HOSTS[@]}; do
+    echo ""
+    echo $i
+    ssh "isucon@${i}" "${APP_COMMAND}"
+  done
+fi
