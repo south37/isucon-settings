3f1253a60adb76654606faea8a88791a2e01d498
Nao Minami <south37777@gmail.com>
2017-11-25 00:06:35 +0900

    Add TAIL_LENGTH option

--- all_scripts/error_log.sh
+++ all_scripts/error_log.sh
@@ -2,6 +2,19 @@
 
 . "$(pwd)/all_scripts/hosts.txt"
 
+TAIL_LENGTH=10
+while [ "$1" != "" ]; do
+  case "$1" in
+    '-n' )
+        TAIL_LENGTH=$2
+        shift 2
+        ;;
+    * )
+        shift 1
+        ;;
+  esac
+done
+
 NGINX_COMMAND="hostname && echo '' && ( /home/isucon/scripts/error_log.sh | tail -n ${TAIL_LENGTH} )"
 for i in ${NGINX_HOSTS[@]}; do
   echo ""
