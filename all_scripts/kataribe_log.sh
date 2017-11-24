0d1f96a26fc4aeb9afc3456418c93dbddf214679
Nao Minami <south37777@gmail.com>
2017-11-24 11:48:15 +0900

    Add kataribe_log.sh

---
+++ all_scripts/kataribe_log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. "$(pwd)/all_scripts/hosts.txt"
+
+BRANCH_COMMAND="git branch | grep '*' | awk '{ print \$2 }'"
+COMMAND="hostname && git pull origin \$(${BRANCH_COMMAND}) && /home/isucon/scripts/rotate_and_cp.sh && git add . && git commit -m 'Add log' && git push origin \$(${BRANCH_COMMAND})"
+
+echo "${COMMAND}"
+for i in ${NGINX_HOSTS[@]}; do
+  echo ""
+  echo $i
+  ssh "isucon@${i}" "${COMMAND}"
+done
+
+git pull origin `git branch | grep '\*' | awk '{ print \$2 }'`
