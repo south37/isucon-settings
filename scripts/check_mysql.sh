ad673afae91e24403ed30a4cfc2995d872b398cf
Kent Nagata <ngtknt@me.com>
2017-11-25 09:48:49 +0900

    Fix new line

--- scripts/check_mysql.sh
+++ scripts/check_mysql.sh
@@ -11,11 +11,15 @@ if [ "$DB_PW" = "" ] || [ "$DB_USER" = "" ] || [ "$DB_NAME" = "" ] ; then
   exit 1
 fi
 
-echo "\n\n## データベース一覧"
+echo ""
+echo ""
+echo "## データベース一覧"
 mysql -u$DB_USER -p$DB_PW -e "SHOW DATABASES";
 
 
-echo "\n\n## テーブル一覧"
+echo ""
+echo ""
+echo "## テーブル一覧"
 for i in $(mysql -u$DB_USER -p$DB_PW -D$DB_NAME -e 'SHOW TABLES' | grep -v "Tables_in" | awk '{print $1}')
 do
   echo "\n\nTable: $i"
@@ -23,13 +27,19 @@ do
 done
 
 
-echo "\n\n## 各テーブルの容量"
+echo ""
+echo ""
+echo "## 各テーブルの容量"
 mysql -u $DB_USER -p$DB_PW $DB_NAME -e "SELECT table_name, engine, table_rows, avg_row_length, floor((data_length+index_length)/1024/1024) as allMB, floor((data_length)/1024/1024) as dMB, floor((index_length)/1024/1024) as iMB FROM information_schema.tables WHERE table_schema=database() ORDER BY (data_length+index_length) DESC;"
 
 
-echo "\n\n## インデックス一覧"
+echo ""
+echo ""
+echo "## インデックス一覧"
 for i in $(mysql -u$DB_USER -p$DB_PW -D$DB_NAME -e 'SHOW TABLES' | grep -v "Tables_in" | awk '{print $1}')
 do
-  echo "\n\nTable: $i"
+  echo ""
+  echo ""
+  echo "Table: $i"
   mysql -u$DB_USER -p$DB_PW -D$DB_NAME -e "SHOW INDEX FROM $i"
 done
